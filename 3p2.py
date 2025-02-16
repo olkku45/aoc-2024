9a007988934406b79588279180ab11591bc2b830
olkku45 <olli.tervonen05@gmail.com>
2025-02-16 10:45:29 +0200

    3 part 2 solved

--- 3p2.py
+++ 3p2.py
@@ -18,77 +18,32 @@ def reconstruct(groups):
 def reading_check(file):
     file_copy = [file[i:i+n] for i in range(len(file))]
     reading = True
-    removed_first = []
-    removed_second = []
+    removed = []
 
-    # DEBUG
-    print(f"(1)Removed First: {len(removed_first)}")
-    print(f"(1)Removed Second: {len(removed_second)}")
-    print(f"(1)Remaining: {len(file_copy)}")
+    while file_copy:
+        part = file_copy.pop(0)
+        removed.append(part)
 
-    while reading:
-        to_remove = []
+        if "don't" in part:
+            reading = False
+        elif "do" in part:
+            reading = True
 
-        for index, part in enumerate(file_copy):
-            removed_first.append(part)
-            to_remove.append(index)
+        if not reading:
+            while file_copy:
+                part = file_copy.pop(0)
+                
+                if "do" in part:
+                    reading = True
+                    break
 
-            if "don't" in part:
-                reading = False
-                break
+    print(removed)
+    reconstructed = reconstruct(removed)
+    return reconstructed    
 
-        for i in reversed(to_remove):
-            del file_copy[i]
-    
-        if len(file_copy) <= 1:
-            return
-        
-    # DEBUG
-    print("(2)Removed First:", len(removed_first))
-    print("(2)Removed Second:", len(removed_second))
-    print("(2)Remaining:", len(file_copy))
-        
-    while not reading:
-        to_remove = []
-
-        #print(list(enumerate(file_copy)))
-        for index, part in enumerate(file_copy):
-            to_remove.append(index)
-
-            if "do" in part:
-                reading = True
-                removed_second.append(part)
-                break
-
-        for i in reversed(to_remove):
-            del file_copy[i]
-        
-        if not file_copy:
-            break
-    
-    # DEBUG
-    print("(3)Removed First:", len(removed_first))
-    print("(3)Removed Second:", len(removed_second))
-    print("(3)Remaining:", len(file_copy))
-
-    while reading and file_copy:
-        removed_second.append(file_copy.pop(0))
-
-    # DEBUG
-    print("(4)Removed First:", len(removed_first))
-    print("(4)Removed Second:", len(removed_second))
-    print("(4)Remaining:", len(file_copy))
-
-    reconstructed = reconstruct(removed_first) + reconstruct(removed_second)
-    return reconstructed
 
 file = reading_check(file)
 number_pairs = re.findall(pattern, file)
-
-# DEBUG
-'''print(f"Extracted text: {file}")  
-print(f"Matches found: {number_pairs}")
-print(f"Reconstructed text: {reading_check(file)}")'''
 
 for pair in number_pairs: 
     pair = list(pair)
