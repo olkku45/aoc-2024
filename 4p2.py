47124fae230be4c26a32f4f66544ffd2dffb3d7b
olkku45 <olli.tervonen05@gmail.com>
2025-02-19 17:52:52 +0200

    4 part 2 solved

--- 4p2.py
+++ 4p2.py
@@ -1,4 +1,3 @@
-pattern = ["MAS", "SAM"]
 lines = []
 
 with open("4.txt", "r") as file:
@@ -6,23 +5,25 @@ with open("4.txt", "r") as file:
         stripped_line = line.rstrip()
         lines.append(stripped_line)
 
-xmas_counter = 0
-found_positions = set()
-
 rows = len(lines)
 cols = len(lines[0])
+xmas_counter = 0
 
-for x in range(rows - 2):
-    for y in range(cols - 2):
+for x in range(rows):
+    for y in range(cols):
         chunk = [line[y:y+3] for line in lines[x:x+3]]
 
         if len(chunk) == 3 and all(len(row) == 3 for row in chunk):
-
-            first_diag = "".join([chunk[0][0], chunk[1][1], chunk[2][2]])
-            second_diag = "".join([chunk[0][2], chunk[1][1], chunk[2][0]])
-
-            if first_diag in pattern or second_diag in pattern:
-                if (x,y) not in found_positions:
+            if chunk[1][1] == "A": 
+                concatenated = chunk[0][0] + chunk[0][2] + chunk[2][0] + chunk[2][2]
+                joined = "".join(concatenated)
+                if joined == "MSMS":
                     xmas_counter += 1
-                    found_positions.add((x,y))
-                    
+                elif joined == "SMSM":
+                    xmas_counter += 1
+                elif joined == "MMSS":
+                    xmas_counter += 1
+                elif joined == "SSMM":
+                    xmas_counter += 1
+
+print(xmas_counter)
