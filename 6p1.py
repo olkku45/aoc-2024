8b0c04863ad2ad54c461b40202b599e4be008972
olkku45 <olli.tervonen05@gmail.com>
2025-02-22 16:36:45 +0200

    6 p1 matrix rotates correctly but wrong answer with puzzle input

--- 6p1.py
+++ 6p1.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+import time
+start_time = time.time()
+
 with open("6.txt", "r") as file:
     matrix = [list(line.strip()) for line in file]
 
@@ -8,57 +11,67 @@ cols = len(matrix[0])
 
 
 def rotate_matrix(matrix):
-    mlength = len(matrix)
-
-    for i in range(mlength // 2):
-        for j in range(i, mlength - i - 1):
-            temp = matrix[i][j]
-
-            # left to top
-            matrix[i][j] = matrix[mlength - j - 1][i]
-            # bottom to left
-            matrix[mlength - j - 1][i] = matrix[mlength - i - 1][mlength - j - 1]
-            # right to bottom
-            matrix[mlength - i - 1][mlength - j - 1] = matrix[j][mlength - i - 1]
-            # right to top
-            matrix[j][mlength - i - 1] = temp
-    
-    return matrix
+    # thanks to https://stackoverflow.com/a/53251028
+    # matrix rotation 90 degrees counter clockwise
+    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0])-1,-1,-1)]
 
 
 flag = False
+first_move = True
 
 while not flag:
     flag = False
+    rotation = False
 
     for x in range(1, rows):
         for y in range(cols):
 
             if matrix[x][y] == "^":
-
+                
                 if matrix[x - 1][y] == "#":
-                    rotate_matrix(matrix)
+                    matrix = rotate_matrix(matrix)
+                    rotation = True
+                    
+                    for i in range(rows - 1): 
+                        for j in range(cols - 1):  # n^4 time complexity is kinda bad tbh but whatever
+                            if matrix[i][j] == "^" and matrix[i][j + 1] == ".":
+                                matrix[i][j + 1] = "X"
+
                     continue
 
-                matrix[x][y], matrix[x - 1][y] = matrix[x - 1][y], matrix[x][y]
-                
-                if matrix[x - 1][y] == ".":
-                    matrix[x - 1][y] = "X"
+                if not rotation and not first_move:  # kind of crude fix, don't add "X" on the first move because then there is an extra X
+                    if matrix[x + 1][y] == ".":
+                        matrix[x + 1][y] = "X"
 
-                for sublist in matrix:
-                    if sublist == matrix[0]:
-                        if "^" in sublist:
-                            matrix[x][y] = "X"
-                            flag = True
+                matrix[x][y], matrix[x - 1][y] = matrix[x - 1][y], matrix[x][y]
+
+                if "^" in matrix[0]:
+                    col_index = matrix[0].index("^")
+                    matrix[0][col_index] = "X"
+
+                    if rows > 1 and matrix[1][col_index] == ".":
+                        matrix[1][col_index] = "X"
+
+                    flag = True
+
+                first_move = False
 
         if flag == True:
             break
         
-    for row in matrix:
+    '''for row in matrix:  # for debugging
         print("".join(row))
-    print("\n" + "-"*20 + "\n") 
+    print("\n" + "-"*20 + "\n")'''
 
+position_counter = 0
 
+for line in matrix:
+    for character in line:
+        if character == "X":
+            position_counter += 1
+
+print(position_counter)
+print("--- %s seconds ---" % (time.time() - start_time))
 
 '''löydetään se 'view', jossa vartija on alempana, eli
 listan toinen alkio, ja jos ensimmäinen alkio on piste,
@@ -81,3 +94,9 @@ myötäpäivään. tehdään kuitenkin tässä sama homma, että
 vartijan ja pisteen paikka vaihdetaan, ja vaihdetun 
 pisteen tilalle laitetaan ruksi. juu voisi tehdä homman
 oikeastaan näin, tulee olemaan paljon helpompaa luulisin'''
+'''for sublist in matrix:
+    if sublist == matrix[0]:
+        if "^" in sublist:
+            matrix[x][y] = "X"
+            flag = True'''
+'''5158 too high'''
