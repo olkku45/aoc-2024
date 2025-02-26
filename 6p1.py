08cbd1de471fa2ec4aa96a8fe903e12b812a4a4e
olkku45 <olli.tervonen05@gmail.com>
2025-02-26 22:03:21 +0200

    6 p1 finally working with junk

--- 6p1.py
+++ 6p1.py
@@ -1,7 +1,9 @@
 import numpy as np
-
+import random
 import time
 start_time = time.time()
+
+random_number = random.randint(1, 1000)
 
 with open("6.txt", "r") as file:
     matrix = [list(line.strip()) for line in file]
@@ -18,50 +20,79 @@ def rotate_matrix(matrix):
 
 flag = False
 first_move = True
+time_from_rotation = 0  # think of better name for this (maybe delete)
+index = 0
 
 while not flag:
-    flag = False
+    flag = False  # flag for checking whether guard has reached the top row
     rotation = False
+    rotation_counter = 0
 
-    for x in range(1, rows):
-        for y in range(cols):
+    for x in range(1, rows):  # x -> y, rows -> cols
+        for y in range(cols):  # y -> x, cols -> rows
 
             if matrix[x][y] == "^":
-                
+
                 if matrix[x - 1][y] == "#":
                     matrix = rotate_matrix(matrix)
                     rotation = True
-                    
-                    for i in range(rows - 1): 
-                        for j in range(cols - 1):  # n^4 time complexity is kinda bad tbh but whatever
-                            if matrix[i][j] == "^" and matrix[i][j + 1] == ".":
-                                matrix[i][j + 1] = "X"
-
+                    rotation_counter += 1
+                    '''print(f"iter. rotation counter: {rotation_counter}")'''
                     continue
 
-                if not rotation and not first_move:  # kind of crude fix, don't add "X" on the first move because then there is an extra X
+                else:
+                    matrix[x][y], matrix[x - 1][y] = matrix[x - 1][y], matrix[x][y]
+                    matrix[x][y] = "X"
+
+                '''if time_from_rotation == 1 and rotation_counter != 2:
+                    if matrix[x + 1][y + 1] == ".":
+                        matrix[x + 1][y + 1] = "X"
+                        time_from_rotation -= 1        
+
+                else:
+                    pass'''
+                
+                '''if rotation_counter == 1 and matrix[x + 1][y] != "X":
+                    #matrix[x + 1][y] = "O"
+                    if matrix[x][y + 1] == ".":
+                        matrix[x][y + 1] = "X"'''
+
+                # change guard's position forward
+                '''matrix[x][y], matrix[x - 1][y] = matrix[x - 1][y], matrix[x][y]'''
+
+                '''if not rotation and not first_move:  # kind of crude fix, don't add "X" on the first move because then there is an extra X
                     if matrix[x + 1][y] == ".":
-                        matrix[x + 1][y] = "X"
+                        matrix[x + 1][y] = "X"'''
 
-                matrix[x][y], matrix[x - 1][y] = matrix[x - 1][y], matrix[x][y]
-
+                # if guard is at the top row
                 if "^" in matrix[0]:
                     col_index = matrix[0].index("^")
                     matrix[0][col_index] = "X"
 
                     if rows > 1 and matrix[1][col_index] == ".":
                         matrix[1][col_index] = "X"
+                    
+                    '''if matrix[x + 1][y + 1] == ".":
+                        matrix[x + 1][y + 1] = "X"'''
 
                     flag = True
 
                 first_move = False
 
+        # break loop after guard has gotten to the top row
         if flag == True:
             break
-        
-    '''for row in matrix:  # for debugging
-        print("".join(row))
-    print("\n" + "-"*20 + "\n")'''
+    
+    index += 1
+
+    if rotation:
+        time_from_rotation += 1
+
+    # DEBUG
+    if index == random_number:  
+        for row in matrix:
+            print("".join(row))
+        print("\n" + "-"*20 + "\n")
 
 position_counter = 0
 
@@ -72,6 +103,19 @@ for line in matrix:
 
 print(position_counter)
 print("--- %s seconds ---" % (time.time() - start_time))
+
+
+'''tässä on varmaan vikana se, että kun
+käännytään, niin lisätään X myöhässä (vasta käännöksen jälkeen)
+siihen paikkaan, ennen paikkaa, josta käännyttiin. voi
+tulla joku rajatapaus, että on # heti kääntymisen jälkeen, 
+jolloin käännytään heti edellisen käännön jälkeen
+eli siis vain käännytään 180 astetta ympäri,
+ja tällöin tulee lisättyä ylimääräinen X, kun ohjelma
+laskee nämä kaksi käännöstä yhdeksi (syistä), jolloin
+kun käännytään ympäri, niin lisätään X yhden askeleen
+alas ja oikealle, jota ei pitäisi tietenkään tehdä, kun
+vartija ei ole käynyt siinä paikassa.'''
 
 '''löydetään se 'view', jossa vartija on alempana, eli
 listan toinen alkio, ja jos ensimmäinen alkio on piste,
